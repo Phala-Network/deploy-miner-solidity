d2c878158d0498d7b066be78618a06e6ab874f34
tolak <kalot.wang@gmail.com>
2024-01-30 14:57:15 +0800

    miner management test

--- packages/foundry/test/MinerManagement.sol
+++ packages/foundry/test/MinerManagement.sol
@@ -5,29 +5,50 @@ import "forge-std/Test.sol";
 import "forge-std/mocks/MockERC20.sol";
 import "../contracts/MinerManagement.sol";
 
+contract Token_ERC20 is MockERC20 {
+    constructor(string memory name, string memory symbol, uint8 decimals) {
+        initialize(name, symbol, decimals);
+    }
+
+    function mint(address to, uint256 value) public virtual {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) public virtual {
+        _burn(from, value);
+    }
+}
+
 contract MinerManagementTest is Test {
     MinerManagement public minerManagement;
-    MockERC20 public usdc;
-    MockERC20 public testToken;
+    Token_ERC20 public usdc;
 
     function setUp() public {
-        usdc = deployMockERC20("USD Coin", "USDC", 6);
-        testToken = deployMockERC20("Test Token", "TT", 18);
+        usdc = new Token_ERC20("USD Coin", "USDC", 6);
+        usdc.mint(DEFAULT_SENDER, 1000000000);
+        assertEq(usdc.totalSupply(), 1000000000);
+        assertEq(usdc.balanceOf(address(DEFAULT_SENDER)), 1000000000);
         // Pay 100 USDC for a worker
         minerManagement = new MinerManagement(DEFAULT_SENDER, address(usdc), 100000000);
     }
 
     function testPayMiningWithUSDC() public {
+        vm.prank(address(DEFAULT_SENDER));
+        usdc.approve(address(minerManagement), 100000000);
+        vm.prank(address(DEFAULT_SENDER));
         bytes32 minerId = minerManagement.payForMining();
 
         require(IERC20(address(usdc)).balanceOf(address(minerManagement)) == 100000000, "Failed to buy worker");
-        require(minerManagement.hasPaied(minerId), "Incorrect miner state");
+        require(minerManagement.hasPaied(minerId), "Miner should be paied");
 
+        vm.prank(address(DEFAULT_SENDER));
         minerManagement.reportMinerOnline(minerId);
-        require(minerManagement.isActived(minerId), "Incorrect miner state");
-        require(!minerManagement.hasExpired(minerId), "Incorrect miner state");
+        require(minerManagement.isActived(minerId), "Miner should be actived");
+        require(!minerManagement.hasExpired(minerId), "Miner should not expired");
 
-        vm.warp(60);
-        require(minerManagement.hasExpired(minerId), "Incorrect miner state");
+        // block.timestamp start with 1, and DEFAULT_EXPIRATION_DURATION is 60, so 62 should
+        // be the expire time
+        vm.warp(62);
+        require(minerManagement.hasExpired(minerId), "Miner should expired");
     }
 }
